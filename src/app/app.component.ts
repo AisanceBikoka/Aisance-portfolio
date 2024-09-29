4c5c234a255cb6af26ffc93290782dc6f0f0aaf1
Aisance Bikoka <aisanceb@gmail.com>
2024-09-29 22:23:22 +0200

    Initialisation du projet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbarModule, MatIconModule, MatButtonModule,HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+   /* {
+      path: '',
+      pathMatch: 'full',
+      redirectTo: 'home'
+    },
+    {
+      path: 'home',
+      component: HomeComponent,
+    },
+    {
+      path: 'about-me',
+      component: AboutMeComponent,
+    },
+    {
+      path: 'achievements',
+      component: AchievementsComponent,
+    },
+    {
+      path: 'contact',
+      component: ContactComponent,
+    },*/
+  
+  ];
